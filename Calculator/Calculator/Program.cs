adc4849c3aaf68ccc4ef6f50c817f6046e1bc79f
ellisken <kse104@gmail.com>
2018-10-08 10:58:46 -0700

    started main to get user input

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,10 +6,25 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            string menu = "Select 1 for Add\nSelect 2 for Subtract\nSelect 3 for Multiple\nSelect 4 for Multiply";
+            int[] userInput = new int[3];
+            
             //Print Menu
-            //Get User Choice
+            Console.WriteLine("Please make a selection:");
+            Console.WriteLine("{0}", menu);
+
+            //Get User
+            userInput[0] = Convert.ToInt32(Console.ReadLine());
+
             //Get User input (operands)
+            Console.WriteLine("Enter in two operands (hit enter after each):");
+            for (int i = 1; i < 3; i++)
+            {
+                userInput[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
             //Return Output
+
         }
 
         static int Add(int x, int y)
