569d49cb3020275fad6eb121280635f79bc20ee7
ellisken <kse104@gmail.com>
2018-10-08 10:47:21 -0700

    started Add and Subtract

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
